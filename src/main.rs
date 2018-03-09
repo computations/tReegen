370890e61f3120e89da8f06821659c6a3c29bb64
Ben Bettisworth <bwbettisworth@alaska.edu>
2018-03-08 20:36:21 -0900

    rewrote it using enums this time

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,70 @@
 extern crate rand;
 use rand::distributions::Exp;
 use rand::distributions::Sample;
+use rand::XorShiftRng;
 use rand::SeedableRng;
 use rand::Rng;
 
-trait Newick{
-    fn to_newick(&self) -> String;
-}
 
 #[derive(Debug)]
-struct Leaf{
-    weight: f64,
-    label: String,
+enum NewickNode{
+    Root{left_child: Box<NewickNode>, right_child: Box<NewickNode>},
+    Node{left_child: Box<NewickNode>, right_child: Box<NewickNode>, weight:f64},
+    Leaf{weight:f64, label:String},
 }
 
-impl Newick for Leaf{
+impl NewickNode{
     fn to_newick(&self) -> String{
-        self.label.to_owned() + ":" + &format!("{:.4}", self.weight)
+        match self{
+            &NewickNode::Leaf{weight, ref label}=>  {
+                label.to_owned() + ":" + &format!("{:.4}", weight)
+            },
+            &NewickNode::Node{left_child: ref lc, right_child: ref rc, weight: w}=>  {
+                "(".to_string() + &lc.to_newick() + "," + &rc.to_newick() + 
+                    "):" + &format!("{:.4}", w)
+            },
+            &NewickNode::Root{left_child: ref lc, right_child: ref rc} =>  {
+                "(".to_string() + &lc.to_newick() + "," + &rc.to_newick() + ")"
+            },
+        }
     }
 }
 
-impl Leaf{
-    fn new<T:Into<String>>(label: T) -> Leaf{
-        let mut rng = rand::thread_rng();
-        let mut e = Exp::new(1.0);
-        let w = e.sample(&mut rng);
-        Leaf{label: label.into(), weight: w}
-    }
+fn new_leaf(l: String) -> NewickNode{
+    NewickNode::Leaf{weight: 0.1234, label:l}
 }
 
-#[derive(Debug)]
-struct Node<T:Newick, U:Newick>{
-    weight: f64,
-    left_child : Box<T>,
-    right_child : Box<U>,
+fn new_node(lc: NewickNode, rc: NewickNode) -> NewickNode{
+    NewickNode::Node{left_child: Box::new(lc), right_child: Box::new(rc), weight:0.13}
 }
 
-impl <T:Newick, U:Newick> Newick for Node<T, U>{
-    fn to_newick(&self) -> String{
-        String::from("(") + &self.left_child.to_newick() + "," + 
-            &self.right_child.to_newick() + "):" + &format!("{:.4}", self.weight)
-    }
+fn new_root(lc: NewickNode, rc: NewickNode) -> NewickNode{
+    NewickNode::Root{left_child: Box::new(lc), right_child: Box::new(rc)}
 }
 
-impl <T:Newick, U:Newick> Node<T,U>{
-    fn new(lc:T , rc:U) -> Node<T, U>{
-        let mut rng = rand::thread_rng();
-        let mut e = Exp::new(1.0);
-        let w = e.sample(&mut rng);
-        Node{left_child: Box::new(lc), right_child: Box::new(rc), weight:w}
+fn gen_tree(tree_size: u8) -> NewickNode{
+    let mut tree = Vec::with_capacity(tree_size as usize);
+    let label_start = 'A' as u8;
+    let label_end = label_start + tree_size;
+    for l in label_start .. label_end{
+        tree.push(new_leaf((l as char).to_string()));
     }
+
+    let mut xorshift = XorShiftRng::from_seed();
+
+    while tree.len() != 2{
+        let c1 = xorshift.gen_range(0, tree.len());
+        let c2 = xorshift.gen_range(0, tree.len());
+        assert!(c1 != c2);
+        let n = new_node(tree.pop().unwrap(), tree.pop().unwrap());
+        tree.push(n);
+    }
+
+    let r = new_root(tree.pop().unwrap(), tree.pop().unwrap());
+    r
 }
 
 fn main(){
-    let mut c = 'A' as u8;
-    let l1 = Leaf::new((c as char).to_string());
-    c+=1;
-    let l2 = Leaf::new((c as char).to_string());
-    let n = Node::new(l1, l2);
-    println!("{:?}", n);
-    println!("{}", n.to_newick());
-    let mut xorshift = rand::XorShiftRng::from_seed([2, 2, 1, 0]);
-    let random_number = xorshift.gen::<u32>();
-    println!("{}", random_number);
+    let t = gen_tree(10u8);
+    println!("{}", t.to_newick());
 }
